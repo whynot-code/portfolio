79133e258b54bc1347ccead7cdce3b86a8c7e0e9
Patryk <whynot.dotslave@gmail.com>
2025-03-18 13:01:55 +0100

    Add portrait image and improve Header component layout

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,31 @@
 import React, { PropsWithChildren } from 'react';
 import styles from './Header.module.css';
+import Portrait from '../assets/portrait-template.webp';
 
 const Header: React.FC<PropsWithChildren> = ({ children }) => {
     return (
         <header className={styles.head}>
-            <img className={styles.img} src="" alt="" />
-            <div className={styles.botom}></div>
+            <div className={styles.img}>
+                <img src={Portrait} alt="Portrait" />
+            </div>
+            <div className={styles.botom}>
+                <h1>Patryk Seroka</h1>
+                <ul className={styles.social}>
+                    <a href="">
+                        <li>Fb</li>
+                    </a>
+                    <a href="">
+                        <li>GH</li>
+                    </a>
+                    <a href="">
+                        <li>In</li>
+                    </a>
+                </ul>
+                <div className={styles.buttons}>
+                    <button type='submit'>Download CV</button>
+                    <button type='submit'>Contact Me</button>
+                </div>
+            </div>
             {children}
         </header>
     );
